a884810a757f8203ff435a325060fb09b3d6aed6
MacobJagyar <jacobmagyar95@gmail.com>
2022-06-29 22:05:13 -0400

    Initial commit

---
+++ BasicForLoopLab/Program.cs
@@ -0,0 +1,28 @@
+﻿string answer;
+
+
+do
+{
+    int sum = 0;
+
+    Console.Write("Hello! Please enter a number: ");
+    int input = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i <= input; i++)
+    {
+        sum += i;
+    }
+
+    Console.Write("\n" + sum);
+
+    Console.Write("\n\nWould you like to try again? y/n: \n");
+    answer = Console.ReadLine();
+} while (answer == "y");
+
+if (answer == "n")
+{
+    Console.WriteLine("\nGoodbye!");
+}
+
+
+Console.ReadKey();
